db964abf16af4fb78662c2c4a5f93cef4f9681e9
Ali Sabet <hialisabet@gmail.com>
2023-05-20 11:40:50 +0330

    _00616_TavanDo

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -23,83 +23,65 @@ namespace Quera
 
         public static void _00589_Factorial()
         {
-            string? str = Console.ReadLine();
-            if (!string.IsNullOrEmpty(str))
-            {
-                int input = int.Parse(str);
-                Console.WriteLine(Factorial(input));
-            }
+            string str = Console.ReadLine();
+            int input = int.Parse(str);
+            Console.WriteLine(Factorial(input));
         }
 
         public static void _00616_TavanDo()
         {
             //https://docs.google.com/document/d/1kcwlIamCsdGc4-HjS2nbYB1JtxlVVm9trfxzVWm-Fns/edit#heading=h.4prh5tu56954
-            string? str = Console.ReadLine();
-            if (!string.IsNullOrEmpty(str))
-            {
-                int input = int.Parse(str);
-                string inputBinary = Convert.ToString(input, 2);
-                string outputBinary = '1' + new string('0', inputBinary.Length);
-                Console.WriteLine(Convert.ToInt32(outputBinary, 2));
-            }
+            string str = Console.ReadLine();
+            int input = int.Parse(str);
+            string inputBinary = Convert.ToString(input, 2);
+            string outputBinary = '1' + new string('0', inputBinary.Length);
+            Console.WriteLine(Convert.ToInt32(outputBinary, 2));
         }
 
         public static void _02885_YekSoaleSade()
         {
-            string? str = Console.ReadLine();
-            if (!string.IsNullOrEmpty(str))
+            string str = Console.ReadLine();
+            int input = int.Parse(str);
+            for (int i = 0; i < input; i++)
             {
-                int input = int.Parse(str);
-                for (int i = 0; i < input; i++)
-                {
-                    Console.WriteLine("man khoshghlab hastam");
-                }
+                Console.WriteLine("man khoshghlab hastam");
             }
         }
 
         public static void _03429_Yakhdarchi()
         {
-            string? str = Console.ReadLine();
-            if (!string.IsNullOrEmpty(str))
+            string str = Console.ReadLine();
+            int input = int.Parse(str);
+            if (input > 100)
             {
-                int input = int.Parse(str);
-                if (input > 100)
-                {
-                    Console.WriteLine("Steam");
-                }
-                else if (input < 0)
-                {
-                    Console.WriteLine("Ice");
-                }
-                else
-                {
-                    Console.WriteLine("Water");
-                }
+                Console.WriteLine("Steam");
+            }
+            else if (input < 0)
+            {
+                Console.WriteLine("Ice");
+            }
+            else
+            {
+                Console.WriteLine("Water");
             }
         }
 
         public static void _03537_SoaleZard()
         {
-            string? str = Console.ReadLine();
-            if (!string.IsNullOrEmpty(str))
+            string str = Console.ReadLine();
+            int input = int.Parse(str);
+            Console.Write("W");
+            for (int i = 0; i < input; i++)
             {
-                int input = int.Parse(str);
-                Console.Write("W");
-                for (int i = 0; i < input; i++)
-                {
-                    Console.Write("o");
-                }
-                Console.Write("w!");
+                Console.Write("o");
             }
+            Console.Write("w!");
         }
 
         public static void _09774_AdadChapKon()
         {
-            string? str = Console.ReadLine();
-            if (!string.IsNullOrEmpty(str))
-            {
-                //...
-            }
+            string str = Console.ReadLine();
+            //...
         }
 
     }
